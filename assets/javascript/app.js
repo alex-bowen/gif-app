2e050709dc9ff380efa1fd3e944f5336bc9b825d
alex-bowen <alexbowen2017@gmail.com>
2018-07-20 14:24:09 -0500

    Fixed issue where some sets of gifs would not animate

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -74,20 +74,22 @@ $(".gif-button").on("click", function () {
                 }
             }
 
-            $(document).on("click", "#click-gif", function () {
-
-                var state = $(this).attr("data-state");
-
-                if (state === "still") {
-                    $(this).attr("src", $(this).attr("data-animate"));
-                    $(this).attr("data-state", "animate");
-                } else {
-                    $(this).attr("src", $(this).attr("data-still"));
-                    $(this).attr("data-state", "still");
-                }
-            });
+    
 
         });
+});
+
+$(document).on("click", "#click-gif", function () {
+
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
 });
 
 
